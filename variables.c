aa1e040fdd3ac4e9421fdd80b17ab1f2237688b6
m0hammad-ali <muhammad098ali.1@gmail.com>
2026-04-05 12:51:29 +0500

    Add variables practice: int, float, and char

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float pi = 3.14;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Pi value: %.2f\n", pi);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
