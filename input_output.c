10521540dd7ae9ba1c72a8b7af7c29534b9558d6
m0hammad-ali <muhammad098ali.1@gmail.com>
2026-04-06 16:40:56 +0500

    Update input_output.c to include float and char inputs

--- input_output.c
+++ input_output.c
@@ -2,10 +2,22 @@
 
 int main() {
     int age;
+    float grade;
+    char initial;
 
     printf("Enter your age: ");
     scanf("%d", &age);
-    printf("You are %d years old.\n", age);
-    
+
+    printf("Enter your GPA: ");
+    scanf("%f", &grade);
+
+    printf("Enter your middle initial: ");
+    scanf(" %c", &initial);
+
+    printf("\n--- User Profile ---\n");
+    printf("Initial: %c\n", initial);
+    printf("Age:     %d years old\n", age);
+    printf("GPA:     %.2f\n", grade);
+
     return 0;
 }
