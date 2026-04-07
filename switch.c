c7af0b2b2163b838d29e98db08aa4312787658be
m0hammad-ali <muhammad098ali.1@gmail.com>
2026-04-07 17:29:30 +0500

    Add switch.c

---
+++ switch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+    printf("Enter day number (1-7): ");
+    scanf("%d", &day);
+
+    switch (day) {
+        case 1:
+            printf("Monday\n");
+            break; 
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+        case 7:
+            printf("Weekend!\n");
+            break;
+        default:
+            printf("Invalid day.\n");
+    }
+
+    return 0;
+}
